8ac60aa9abf545a744810bd419b2e67e22e7efc7
Rudra Bahadur Karki <karkirudra856@gmail.com>
2025-08-08 13:50:51 +0545

    Your commit message

--- BruteForce/app.py
+++ BruteForce/app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 import mysql.connector
 import hashlib
 import os 
+from urllib.parse import quote
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -34,6 +35,11 @@ def register():
         username = request.form['UserName'] 
         password = hash_password(request.form['Password'])
 
+        if not username or not password:
+            msg = quote("username or password cannot be empty.")
+            return redirect(f"/register?msg=(msg)")
+        
+        #exception
         try:
             conn = get_db_connection()
             cur = conn.cursor()
@@ -41,19 +47,56 @@ def register():
             conn.commit()
             cur.close()
             conn.close()
+            msg = quote("Registeration successful! Please log in")
+            return redirect(f"/login?msg={msg}")
+            
             flash("User Registered Successfully", "success")
             return redirect(url_for('login'))  
         except Exception as e:
-            flash(f"Error: {e}", "danger")
+            msg = quote(f"Error:{e}")
+            return redirect(f"/register?msg={msg}")
+
 
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login(): 
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        action = request.form.get('action')
+
+        if not username or not password:
+            msg = quote("Username and password required")
+            return redirect(f"/login?msg={msg}")
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT Password FROM Users WHERE username = %s", (username,))
+        row = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        if not row:
+            msg = quote("User not found")
+            return redirect(f"/login?msg={msg}")
+
+        stored_hash = row[0]
+
+        if action == 'login':
+            if hash_password(password) == stored_hash:
+                session['username'] = username
+                msg = quote("Logged in Successfully!!")
+                return redirect(f"/")
+            else:
+                msg = quote("Incorrect username or password!!")
+                return redirect(f"/login?msg={msg}")
+
     return render_template("login.html")
 
-@app.route('/crack')
+
+@app.route('/crack', methods=['GET', 'POST'])
 def crack(): 
     return render_template("crack.html")
 
