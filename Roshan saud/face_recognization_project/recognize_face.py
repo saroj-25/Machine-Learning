2a8869d554fcabea06e585f52ecb5e4153657ef2
Roshan-saud <saudroshan2739@gmail.com>
2025-08-19 09:57:34 +0545

    refactor: remove unnecessary code

--- Roshan saud/face_recognization_project/recognize_face.py
+++ Roshan saud/face_recognization_project/recognize_face.py
@@ -40,48 +40,6 @@ if not cap.isOpened:
 
 print("Enter q for quit:")
 
-# while True:
-#     ret, frame = cap.read()
-
-#     if not ret:
-#         continue
-
-#     rgb = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-#     # Detect face 
-#     boxes = face_recognition.face_locations(rgb)
-#     encodings = face_recognition.face_encodings(rgb, boxes)
-
-#     names = [] 
-
-#     for encoding in encodings:
-#         matches = face_recognition.compare_faces(known_encoding, encoding)
-
-#         name = 'Unknown'
-#         if True in matches:
-#             matched_idxs = [i for i, b in enumerate(matches) if b]
-#             counts = {}
-#             for i in matched_idxs:
-#                 name = known_names[i]
-#                 counts[name] = counts.get(name, 0) +  1
-            
-#             if counts:
-#                 name = max(counts, key=counts.get)
-#                 names.append(name)
-
-    
-#     # Draw rectangle on faces 
-#     for((top, right, bottom, left), name) in zip(boxes, names):
-#         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-#         cv2.putText(frame, name, (left, top-10), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 2)
-
-    
-
-#     cv2.imshow("Face recognition", frame)
-
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-        # break
-
 while True:
     ret, frame = cap.read()
     if not ret:
