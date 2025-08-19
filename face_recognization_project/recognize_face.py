d9683f6e2b5906de2c5b2fc23930088ba0c1f7cc
Rudra Bahadur Karki <karkirudra856@gmail.com>
2025-08-19 08:02:28 +0545

    Machine learning uptodate

---
+++ face_recognization_project/recognize_face.py
@@ -0,0 +1,86 @@
+import os
+import cv2
+import face_recognition
+
+# Get the project folder
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Dataset directory
+DATASET_DIR = os.path.join(BASE_DIR, "dataset")
+
+# 1. Load all the data from dataset
+known_encodings = []
+known_names = []
+
+for user_name in os.listdir(DATASET_DIR):
+    user_folder = os.path.join(DATASET_DIR, user_name)
+    
+    
+
+    if not  os.path.isdir(user_folder):
+        continue
+        
+        
+    for file_name in os.listdir(user_folder):
+        filepath = os.path.join(user_folder, file_name)
+
+            
+        image = face_recognition.load_image_file(filepath)
+        encodings = face_recognition.face_encodings(image)
+
+        if encodings:
+            known_encodings.append(encodings[0])
+            known_names.append(user_name)
+
+print("Loading data from dataset process completed!")
+
+# Start webcam
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    raise IOError(" Cannot open webcam")
+
+print("Press 'q' to quit")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    # Convert BGR to RGB
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Detect face locations and encodings
+    boxes = face_recognition.face_locations(rgb)
+    encodings = face_recognition.face_encodings(rgb, boxes)
+
+    names = []
+
+    for encoding in encodings:
+        matches = face_recognition.compare_faces(known_encodings, encoding)
+        name = "Unknown"
+
+        if True in matches:
+            matched_idxs = [i for i, b in enumerate(matches) if b]
+            counts = {}
+
+            for i in matched_idxs:
+                 counts[known_names[i]] = counts.get(known_names[i], 0) + 1
+            if counts:
+                name = max(counts, key=counts.get)
+        names.append(name)
+
+    #draw rectangle on face
+    
+    for ((top, right, bottom, left), name) in zip(boxes, names):
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_COMPLEX,0.8, (255, 255, 255), 2)
+
+
+    cv2.imshow("Face Recognition", frame)
+
+    
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ face_recognization_project/register_face.py
@@ -0,0 +1,86 @@
+'''
+Title: Face Detection and Recognition Project
+Features: 
+ - User can feed their image
+ - Store user images dataset/username/username1.png 
+ - Later on we use that image for face recognition
+'''
+
+import cv2
+import face_recognition
+import os
+import pickle
+
+# Get current directory of user
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print(BASE_DIR)
+
+# Dataset and encoding inside the project
+DATASET_DIR = os.path.join(BASE_DIR, "dataset")
+ENCODING_FILE = os.path.join(BASE_DIR, "encodings.pkl")
+
+# Load dataset if already available
+if os.path.exists(ENCODING_FILE):
+    with open(ENCODING_FILE, "rb") as f:
+        known_encoding, known_name = pickle.load(f)
+else:
+    known_encoding, known_name = [], []
+
+# Get name from user
+name = input("Enter your name: ")
+
+# Create subfolder directory with username
+user_dir = os.path.join(DATASET_DIR, name)
+if not os.path.exists(user_dir):
+    os.makedirs(user_dir)
+
+cap = cv2.VideoCapture(0)
+print("Enter 'c' for capture and 'q' for quit")
+counter = 0
+
+while True:
+    
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to capture frame")
+        break
+
+    cv2.imshow("Registered frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('c'):  # ord: convert into ASCII
+        # Recognize RGB value for face
+        rgb = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        boxes = face_recognition.face_locations(rgb)
+
+        if len(boxes) == 0:
+            print("No face detected, try again!")
+            continue
+
+        for box in boxes:
+            top, right, bottom, left = box
+            face_img = frame[top:bottom, left:right]
+
+        # Save the face inside folder
+        filepath = os.path.join(user_dir, f"{name}_{counter}.png")
+        cv2.imwrite(filepath, face_img)
+
+        # Encode face so that machine will understand
+        encodings = face_recognition.face_encodings(rgb, [box])
+        if encodings:
+            known_encoding.append(encodings[0])
+            known_name.append(name)
+            print(f"Saved face {counter} for {name}")
+        counter += 1
+
+    elif key == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+# Save encoding inside project
+with open(ENCODING_FILE, "wb") as f:
+    pickle.dump((known_encoding, known_name), f)
+
+print("Encoding done and saved inside project folder")
